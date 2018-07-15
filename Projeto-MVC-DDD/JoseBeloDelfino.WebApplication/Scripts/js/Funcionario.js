94103d2fa2d91acf95367f89c80b7dc53afd7b9a
jbelodelf <jbelodelf@gmail.com>
2018-07-15 19:16:30 -0300

    Add files via upload

---
+++ Projeto-MVC-DDD/JoseBeloDelfino.WebApplication/Scripts/js/Funcionario.js
@@ -0,0 +1,245 @@
+﻿"use script";
+var Funcionario = {
+    Listar: function () {
+        var nome = $("#NomePesquisar").val();
+
+        var div = $("#ListarFuncionarios");
+        div.empty();
+
+        var token = localStorage.getItem('jtoken');
+        var url = "/Funcionario/ListarFuncionarios";
+        $.ajax({
+            url: url
+            , datatype: "json"
+            , type: "GET"
+            , async: false
+            , data: { Nome: nome, Token: token }
+            , cache: false
+        }).done(function (data) {
+            if (data == "Unauthorized") {
+                localStorage.removeItem('jtoken');
+                window.location.href = "/Login/Login";
+            }
+
+            div.html(data);
+
+         }).fail(function (jqXHR, exception) {
+            TratamentoDeErro(jqXHR, exception)
+        });
+    },
+
+    NovoFuncionario: function () {
+        var token = localStorage.getItem('jtoken');
+        var id = $("#IdFuncionario").val()
+        window.location.href = "/Funcionario/Create?Token=" + token + "&Id=" + id;
+    },
+
+    Deletar: function (id) {
+        bootbox.confirm({
+            message: 'Deseja realmente excluir este registro?',
+            buttons: {
+                confirm: {
+                    label: 'sim',
+                    className: 'btn btn-primary botao-confirmar-sim',
+                },
+                cancel: {
+                    label: 'não',
+                    className: 'btn btn btn-danger botao-confirmar-nao',
+                }
+            },
+            callback: function (result) {
+                if (!result) {
+                    return;
+                }
+
+                var token = localStorage.getItem('jtoken');
+                var url = "/Funcionario/Delete";
+                $.ajax({
+                    url: url
+                    , datatype: "json"
+                    , type: "GET"
+                    , async: false
+                    , data: { Id: id, Token: token }
+                    , cache: false
+                }).done(function (data) {
+                    if (data.retorno == "Unauthorized") {
+                        localStorage.removeItem('jtoken');
+                        window.location.href = "/Login/Login";
+                    }
+                    else {
+                        window.location.href = "/Funcionario/Index";
+                    }
+
+                }).fail(function (jqXHR, exception) {
+                    TratamentoDeErro(jqXHR, exception)
+                });
+            }
+        })
+    },
+
+    Editar: function (id) {
+        var token = localStorage.getItem('jtoken');
+        window.location.href = "/Funcionario/Edit?Id=" + id + "&token=" + token;
+    },
+
+    SalvarFuncionario: function () {
+        Funcionario.Validar();
+        var isValid = $("#frmFuncionario").valid();
+        if (!isValid) { return }
+
+        bootbox.confirm({
+            message: 'Deseja gravar os dados?',
+            buttons: {
+                confirm: {
+                    label: 'sim',
+                    className: 'btn btn-primary botao-confirmar-sim',
+                },
+                cancel: {
+                    label: 'não',
+                    className: 'btn btn btn-danger botao-confirmar-nao',
+                }
+            },
+            callback: function (result) {
+                if (!result) {
+                    return;
+                }
+                var funcionarioViewModel = {
+                    Nome: $("#Nome").val()
+                    , Cpf: $("#Cpf").val()
+                    , Rg: $("#Rg").val()
+                    , Endereco: $("#Endereco").val()
+                    , Cep: $("#Cep").val()
+                    , Cidade: $("#Cidade").val()
+                    , Estado: $("#Estado").val()
+                    , Pais: $("#Pais").val()
+                    , IdSupervisor: $("#IdSupervisor").val()
+                    , IdDepartamento: $("#IdDepartamento").val()
+                    , IdCargo: $("#IdCargo").val()
+                    , IdFuncionario: $("#IdFuncionario").val()
+                }
+
+                var token = localStorage.getItem('jtoken');
+                var url = "/Funcionario/Salvar";
+                $.ajax({
+                    url: url
+                    , datatype: "json"
+                    , type: "POST"
+                    , async: false
+                    , data: { funcionario: funcionarioViewModel, Token: token }
+                    , cache: false
+                }).done(function (data) {
+                    if (data.retorno == "Unauthorized") {
+                        localStorage.removeItem('jtoken');
+                        window.location.href = "/Login/Login";
+                    }
+                    else {
+                        window.location.href = "/Funcionario/Index";
+                    }
+
+                }).fail(function (jqXHR, exception) {
+                    TratamentoDeErro(jqXHR, exception)
+                });
+            }
+        });
+    },
+
+    VoltarFuncionario: function () {
+        window.location.href = "/Funcionario/Index";
+    },
+
+    Validar: function () {
+        var validator = $("#frmFuncionario").validate();
+        validator.destroy();
+
+        $("#frmFuncionario").validate({
+           ignore: [],
+           rules: {
+                Nome: {
+                    required: true,
+                },
+                Cpf: {
+                    required: true,
+                },
+                Rg: {
+                    required: true,
+                },
+                Endereco: {
+                    required: true,
+                },
+                Cep: {
+                    required: true,
+                },
+                Cidade: {
+                    required: true,
+                },
+                Estado: {
+                    required: true,
+                },
+                Pais: {
+                    required: true,
+                },
+                ComboCargo: {
+                   required: true,
+                },
+                ComboDepartamento: {
+                   required: true,
+                },
+                ComboSupervisor: {
+                   required: true,
+                }
+            },
+            messages: {
+                Nome: {
+                    required: "Nome é obrigatória!!!",
+                },
+                Cpf: {
+                    required: "CPF é obrigatória!!!",
+                },
+                Rg: {
+                    required: "RG é obrigatória!!!",
+                },
+                Endereco: {
+                    required: "Endereço é obrigatória!!!",
+                },
+                Cep: {
+                    required: "CEP é obrigatória!!!",
+                },
+                Cidade: {
+                    required: "Cidade é obrigatória!!!",
+                },
+                Estado: {
+                    required: "Estado é obrigatória!!!",
+                },
+                Pais: {
+                    required: "País é obrigatória!!!",
+                },
+                ComboCargo: {
+                    required: "Cargo é obrigatória!!!",
+                },
+                ComboDepartamento: {
+                    required: "Departamento é obrigatória!!!",
+                },
+                ComboSupervisor: {
+                    required: "Supervisor é obrigatória!!!",
+                }
+
+            }
+        });
+    }
+}
+
+$(document).ready(function () {
+    $("#btPesquisar").on("click", function () {
+        Funcionario.Listar();
+    });
+
+    $("#btNovo").on("click", function () {
+        Funcionario.NovoFuncionario();
+    });
+
+    $("#btSalvar").on("click", function () {
+        Funcionario.SalvarFuncionario();
+    });
+
+    $("#btPesquisar").trigger("click");
+})
---
+++ Projeto-MVC-DDD/JoseBeloDelfino.WebApplication/Scripts/js/Login.js
@@ -0,0 +1,144 @@
+﻿"use script";
+var Login = {
+
+    Logar: function (login, senha) {
+        localStorage.getItem('jtoken', '');
+        var url = "/Login/Login";
+
+        $.ajax({
+            url: url
+            , datatype: "json"
+            , type: "POST"
+            , async: false
+            , data: { LoginFuncionario: login, SenhaHash: senha }
+            , cache: false
+        }).done(function (data) {
+
+            var login = {
+                grant_type: 'password',
+                username: data.Login,
+                password: data.SenhaHash
+            }
+            $.ajax({
+                url: "http://localhost:21963/api/autentication/token",
+                type: 'post',
+                contentType: 'x-www-form-urlencoded',
+                data: login
+            }).done(function (data) {
+                localStorage.setItem('jtoken', data.access_token);
+                window.setTimeout(function () {
+                    window.location = "/Funcionario/Index?Token=" + data.access_token;
+                }, 3000);
+            })
+            .fail(function (data) {
+                Mensagem("Funcionário ou Senha inválida!!!", "Erro");
+            });
+        }).fail(function (jqXHR, exception) {
+            TratamentoDeErro(jqXHR, exception)
+        });
+    }
+}
+
+$(document).ready(function () {
+
+    $("#btLogar").click(function () {
+        var login = $("#LoginFuncionario").val();
+        var senha = $("#SenhaHash").val();
+        var loginValido = false;
+        var sanhaValido = false;
+
+        if (login == "") {
+            $("#LoginFuncionario").css({ 'border': '1px solid #FF0000' });
+            Mensagem("Login e senha são obrigatórios!", "Erro");
+        }
+        else {
+            $("#LoginFuncionario").css({ 'border': '1px solid #D8D8D8' });
+            loginValido = true;
+        }
+        if (senha == "") {
+            $("#SenhaHash").css({ 'border': '1px solid #FF0000' });
+            Mensagem("Login e senha são obrigatórios!", "Erro");
+        }
+        else {
+            $("#SenhaHash").css({ 'border': '1px solid #D8D8D8' });
+            sanhaValido = true;
+        }
+
+        if (loginValido && sanhaValido) {
+            Login.Logar(login, senha);
+        }
+    });
+
+    $("#LoginFuncionario").blur(function () {
+        $("#LoginFuncionario").css({ 'border': '1px solid #D8D8D8' });
+    })
+
+    $("#SenhaHash").blur(function () {
+        $("#SenhaHash").css({ 'border': '1px solid #D8D8D8' });
+    })
+})
+
+function Mensagem(mensagem, tipo, time, funcionalidade) {
+    if (tipo == "" || tipo == undefined) {
+        tipo = "erro";
+    }
+
+    var div = $("#mensagem");
+
+    if (($("#myModalContent").data('bs.modal') || {}).isShown) {
+        div = $("#mensagemModal");
+    }
+
+    if ($("#mySidenav").css("width") == "400px") {
+        div = $("#mensagemNav");
+    }
+
+    if (($("#myModal").data('bs.modal') || {}).isShown) {
+        div = $("#mensagemModalDelete");
+    }
+
+    div.html("");
+    div.removeClass();
+
+    switch (tipo.toLowerCase()) {
+        case "sucesso":
+            div.addClass("alert alert-success");
+            break;
+        case "info":
+            div.addClass("alert alert-info");
+            break;
+        case "erro":
+            div.addClass("alert alert-danger");
+            break;
+        case "aviso":
+            div.addClass("alert alert-warning");
+            break;
+    }
+
+    if (time === undefined) time = 10000;
+
+    if (time == 0) {
+        div.html(mensagem)
+            .show();
+    } else {
+        div.html(mensagem)
+            .show()
+            .fadeOut(time);
+    }
+
+    location.href = "#";
+    location.href = "#mensagem";
+}
+
+function ValidarElementoCustomizadoSimples(element, error, valid) {
+    if (valid) {
+        $(element).closest('.form-group').removeClass('has-error').addClass('has-success');
+    } else {
+        var span = $("<span>");
+        span.addClass("help-error");
+        span.html(error);
+
+        $(element).closest('.form-group').removeClass('has-success').addClass('has-error');
+    }
+}
+
