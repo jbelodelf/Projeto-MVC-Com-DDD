1f678a3de465e192f33f00b039bfb2d9ef2cf8d1
jbelodelf <jbelodelf@gmail.com>
2018-07-15 18:54:29 -0300

    Add files via upload

---
+++ Projeto-MVC-DDD/JoseBeloDelfino.Business/Interfaces/IUsuarioNegocio.cs
@@ -0,0 +1,10 @@
+﻿using JoseBeloDelfino.Domain.Entidades;
+using System.Collections.Generic;
+
+namespace JoseBeloDelfino.Business.Interfaces
+{
+    public interface IUsuarioNegocio
+    {
+        List<UsuarioEntity> ListarUsuarios();
+    }
+}
---
+++ Projeto-MVC-DDD/JoseBeloDelfino.Business/Repositorios/UsuarioNegocio.cs
@@ -0,0 +1,17 @@
+﻿using JoseBeloDelfino.Domain.Entidades;
+using JoseBeloDelfino.Domain.Interfaces;
+using System.Collections.Generic;
+
+namespace JoseBeloDelfino.Business.Repositorios
+{
+    public class UsuarioNegocio
+    {
+        private IUsuarioRepositorio usuarioRepositorio;
+
+        public List<UsuarioEntity> ListarUsuarios()
+        {
+            var usuarios = usuarioRepositorio.ListarUsuarios();
+            return usuarios;
+        }
+    }
+}
