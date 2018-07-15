fb49d14197bd33b554df037f714b9f9558ecac4a
jbelodelf <jbelodelf@gmail.com>
2018-07-15 19:11:54 -0300

    Add files via upload

---
+++ Projeto-MVC-DDD/JoseBeloDelfino.WebApplication/Controllers/FuncionarioController.cs
@@ -0,0 +1,71 @@
+﻿using JoseBeloDelfino.Negocio.InterfacesApp;
+using JoseBeloDelfino.ViewModels;
+using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace JoseBeloDelfino.WebApplication.Controllers
+{
+    public class FuncionarioController : Controller
+    {
+        private IFuncionarioNegocio _funcionarioNegocio;
+
+        public FuncionarioController(IFuncionarioNegocio funcionarioNegocio)
+        {
+            _funcionarioNegocio = funcionarioNegocio;
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult ListarFuncionarios(string Token, string nome = "")
+        {
+            var listaFuncionarios = new List<FuncionarioViewModel>();
+            listaFuncionarios = _funcionarioNegocio.ListarFuncionarios(nome, Token);
+            if (Retorno.WebApi.Status == "Unauthorized")
+                return Json(Retorno.WebApi.Status, JsonRequestBehavior.AllowGet);
+
+            return PartialView("~/Views/Funcionario/_ListarFuncionarios.cshtml", listaFuncionarios);
+        }
+
+        public ActionResult Create(string Token, int Id = 0)
+        {
+            ViewBag.Title = "Cadastrar Funcionario";
+
+            FuncionarioViewModel funcionario = new FuncionarioViewModel();
+            funcionario = _funcionarioNegocio.ObterFuncionario(Id, Token);
+            return View("~/Views/Funcionario/FormFuncionario.cshtml", funcionario);
+        }
+
+        [HttpGet]
+        public ActionResult Edit(int Id, string Token)
+        {
+            ViewBag.Title = "Alterar Funcionario";
+
+            var funcionario = _funcionarioNegocio.ObterFuncionario(Id, Token);
+            if (Retorno.WebApi.Status == "Unauthorized")
+                return Redirect("http://localhost:4485/");
+
+            return View("~/Views/Funcionario/FormFuncionario.cshtml", funcionario);
+        }
+
+        [HttpGet]
+        public ActionResult Delete(int Id, string Token)
+        {
+            var retorno = _funcionarioNegocio.DeleteFuncionario(Id, Token);
+            if (Retorno.WebApi.Status == "Unauthorized")
+                return Redirect("http://localhost:4485/");
+
+            return Json(new { mensagem = retorno, retorno = Retorno.WebApi.Status }, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        public ActionResult Salvar(FuncionarioViewModel funcionario, string Token)
+        {
+            var retorno = _funcionarioNegocio.SalvarFuncionario(funcionario, Token);
+            return Json(new { mensagem = "Registro gravado com sucesso!!!", retorno = Retorno.WebApi.Status }, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ Projeto-MVC-DDD/JoseBeloDelfino.WebApplication/Controllers/LoginController.cs
@@ -0,0 +1,55 @@
+﻿using JoseBeloDelfino.Negocio.InterfacesApp;
+using JoseBeloDelfino.ViewModels;
+using JoseBeloDelfino.WebApplication.Models;
+using System;
+using System.Security.Cryptography;
+using System.Text;
+using System.Web.Mvc;
+
+namespace JoseBeloDelfino.WebApplication.Controllers
+{
+    public class LoginController : Controller
+    {
+        private IFuncionarioNegocio _funcionarioNegocio;
+
+        public LoginController(IFuncionarioNegocio funcionarioNegocio)
+        {
+            _funcionarioNegocio = funcionarioNegocio;
+        }
+
+        // GET: Login
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(LoginViewModel loginViewModel, string Token)
+        {
+            var senhaHash = ConvertHash(loginViewModel.SenhaHash);
+            return Json(new { Login = loginViewModel.LoginFuncionario, SenhaHash = senhaHash }, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        public ActionResult Logout()
+        {
+            Usuario.Logado = null;
+            return RedirectToAction("Login");
+        }
+
+        private string ConvertHash(string senha)
+        {
+            SHA256Managed hashstring = new SHA256Managed();
+            byte[] bytes = Encoding.ASCII.GetBytes(Convert.ToString(senha));
+            byte[] hash = hashstring.ComputeHash(bytes);
+            string hashString = string.Empty;
+            foreach (byte x in hash)
+            {
+                hashString += String.Format("{0:x2}", x);
+            }
+
+            return hashString;
+        }
+
+    }
+}
