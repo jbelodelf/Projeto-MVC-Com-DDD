6a39196cc79082b1f16aa59ceba0869ec4ab380f
jbelodelf <jbelodelf@gmail.com>
2018-07-15 18:54:07 -0300

    Add files via upload

---
+++ Projeto-MVC-DDD/JoseBeloDelfino.Application/InterfacesNegocio/IFuncionarioNegocio.cs
@@ -0,0 +1,13 @@
+﻿using JoseBeloDelfino.ViewModels;
+using System.Collections.Generic;
+
+namespace JoseBeloDelfino.Negocio.InterfacesApp
+{
+    public interface IFuncionarioNegocio
+    {
+        FuncionarioViewModel ObterFuncionario(int id, string token);
+        List<FuncionarioViewModel> ListarFuncionarios(string nome, string Token);
+        FuncionarioViewModel SalvarFuncionario(FuncionarioViewModel funcionario, string token);
+        string DeleteFuncionario(int id, string token);
+    }
+}
---
+++ Projeto-MVC-DDD/JoseBeloDelfino.Application/RepositoriesNegocio/FuncionarioNegocio.cs
@@ -0,0 +1,37 @@
+﻿using JoseBeloDelfino.Negocio.InterfacesApp;
+using JoseBeloDelfino.Services.Interfaces;
+using JoseBeloDelfino.ViewModels;
+using System.Collections.Generic;
+
+namespace JoseBeloDelfino.Negocio.RepositoriesApp
+{
+    public class FuncionarioNegocio : IFuncionarioNegocio
+    {
+        private IFuncionarioServiceRepositorio _repositorio;
+
+        public FuncionarioNegocio(IFuncionarioServiceRepositorio repositorio)
+        {
+            _repositorio = repositorio;
+        }
+
+        public List<FuncionarioViewModel> ListarFuncionarios(string nome, string Token)
+        {
+            return _repositorio.ListarFuncionarios(nome, Token);
+        }
+
+        public FuncionarioViewModel ObterFuncionario(int id, string token)
+        {
+            return _repositorio.ObterFuncionario(id, token);
+        }
+
+        public FuncionarioViewModel SalvarFuncionario(FuncionarioViewModel funcionario, string token)
+        {
+            return _repositorio.SalvarFuncionario(funcionario, token);
+        }
+
+        public string DeleteFuncionario(int id, string token)
+        {
+            return _repositorio.DeleteFuncionario(id, token);
+        }
+    }
+}
