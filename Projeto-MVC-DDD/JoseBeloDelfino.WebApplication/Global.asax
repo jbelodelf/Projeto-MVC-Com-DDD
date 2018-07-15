c3e40e858072ad758c1ef72786da91010d4407cd
jbelodelf <jbelodelf@gmail.com>
2018-07-15 19:00:40 -0300

    Add files via upload

---
+++ Projeto-MVC-DDD/JoseBeloDelfino.WebApplication/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="JoseBeloDelfino.WebApplication.MvcApplication" Language="C#" %>
---
+++ Projeto-MVC-DDD/JoseBeloDelfino.WebApplication/Global.asax.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using JoseBeloDelfino.WebApplication.App_Start;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+using System.Web.Http;
+
+namespace JoseBeloDelfino.WebApplication
+{
+    public class MvcApplication : HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            Mapper.Initialize(x => x.AddProfile(new AutoMapperConfig()));
+        }
+    }
+}
