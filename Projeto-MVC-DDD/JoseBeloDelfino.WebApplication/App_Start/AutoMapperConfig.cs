6cd36c9addb33c3ba87e89407f30ce6564101596
jbelodelf <jbelodelf@gmail.com>
2018-07-15 19:01:40 -0300

    Add files via upload

---
+++ Projeto-MVC-DDD/JoseBeloDelfino.WebApplication/App_Start/AutoMapperConfig.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using JoseBeloDelfino.Domain.Entidades;
+using JoseBeloDelfino.DTOs;
+using JoseBeloDelfino.ViewModels;
+
+namespace JoseBeloDelfino.WebApplication.App_Start
+{
+    public class AutoMapperConfig : Profile
+    {
+        public AutoMapperConfig()
+        {
+            CreateMap<LoginDTO, LoginEntity>().ReverseMap();
+            CreateMap<FuncionarioDTO, FuncionarioEntity>().ReverseMap();
+            //CreateMap<AmigoDTO, AmigoEntity>().ReverseMap();
+
+            CreateMap<LoginDTO, LoginViewModel>().ReverseMap();
+            CreateMap<FuncionarioDTO, FuncionarioViewModel>().ReverseMap();
+            //CreateMap<AmigoDTO, AmigoViewModel>().ReverseMap();
+        }
+    }
+}
